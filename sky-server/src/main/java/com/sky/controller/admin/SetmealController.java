e123fdf756a197479a603d37b602c8e355d5d71d
tkwm <liwencang807@163.com>
2025-10-23 17:50:54 +0800

    修改套餐，根据套餐id查询

--- sky-server/src/main/java/com/sky/controller/admin/SetmealController.java
+++ sky-server/src/main/java/com/sky/controller/admin/SetmealController.java
@@ -5,6 +5,7 @@ import com.sky.dto.SetmealPageQueryDTO;
 import com.sky.result.PageResult;
 import com.sky.result.Result;
 import com.sky.service.SetmealService;
+import com.sky.vo.SetmealVO;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
@@ -40,6 +41,20 @@ public class SetmealController {
     public Result delete(@RequestParam List<Long> ids){
         System.out.println(ids);
         setmealService.deleteBatch(ids);
+        return Result.success();
+    }
+
+    @ApiOperation("根据id查询套餐")
+    @GetMapping("/{id}")
+    public Result<SetmealVO> getById(@PathVariable Long id){
+        SetmealVO setmealVO = setmealService.getSetmealWithDishs(id);
+        return Result.success(setmealVO);
+    }
+
+    @ApiOperation("修改套餐")
+    @PutMapping
+    public Result update(@RequestBody SetmealDTO setmealDTO){
+        setmealService.update(setmealDTO);
         return Result.success();
     }
 }
--- sky-server/src/main/java/com/sky/service/SetmealService.java
+++ sky-server/src/main/java/com/sky/service/SetmealService.java
@@ -3,6 +3,7 @@ package com.sky.service;
 import com.sky.dto.SetmealDTO;
 import com.sky.dto.SetmealPageQueryDTO;
 import com.sky.result.PageResult;
+import com.sky.vo.SetmealVO;
 
 import java.util.List;
 
@@ -13,4 +14,8 @@ public interface SetmealService {
     PageResult page(SetmealPageQueryDTO setmealPageQueryDTO);
 
     void deleteBatch(List<Long> ids);
+
+    void update(SetmealDTO setmealDTO);
+
+    SetmealVO getSetmealWithDishs(Long id);
 }
